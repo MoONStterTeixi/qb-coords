4cd84533efbba5fdc49e23c9bb656a7a1706f165
Jaime Teixidó || MoONStter Teixi <jteixidof@gmail.com>
2021-04-10 23:01:19 +0200

    Base

---
+++ qb-coords/server.lua
@@ -0,0 +1,7 @@
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+
+QBCore.Commands.Add("coords", "See map coords", {}, false, function(source, args)
+        TriggerClientEvent("qb-coords_client:enablecoords", -1)
+    end)
