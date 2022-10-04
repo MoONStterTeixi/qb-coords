9cbe02f7965f61984c2ec041af7f88f6ea9f3bcb
Jaime Teixidó || Teixi <jteixidof@gmail.com>
2022-10-04 09:17:00 +0200

    Update client.lua

--- qb-coords/client.lua
+++ qb-coords/client.lua
@@ -4,17 +4,15 @@ TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 local open = false
 
 Citizen.CreateThread(function()
-        while true do
+        while open do
         Citizen.Wait(0)
-                if open then
-                        local posx2,posy2,posz2 = table.unpack(GetEntityCoords(GetPlayerPed(-1),false))
-                        SetTextFont(0)
-                        SetTextScale(0.0, 0.4)
-                        SetTextColour(247, 223, 88, 255)
-                        SetTextEntry("STRING")
-                        AddTextComponentString("x: " .. tonumber(string.format("%.3f", posx2)) .. " y: " .. tonumber(string.format("%.3f", posy2)) .. " z: " .. tonumber(string.format("%.3f", posz2)))
-                        DrawText(0.0001, 0.0001)
-                end
+                local posx2,posy2,posz2 = table.unpack(GetEntityCoords(GetPlayerPed(-1),false))
+                SetTextFont(0)
+                SetTextScale(0.0, 0.4)
+                SetTextColour(247, 223, 88, 255)
+                SetTextEntry("STRING")
+                AddTextComponentString("x: " .. tonumber(string.format("%.3f", posx2)) .. " y: " .. tonumber(string.format("%.3f", posy2)) .. " z: " .. tonumber(string.format("%.3f", posz2)))
+                DrawText(0.0001, 0.0001)
         end
 end)
 
