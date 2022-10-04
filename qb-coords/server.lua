1db8d6039a06906ac8314c6d0a43234acb23c1d5
Jaime Teixidó || Teixi <jteixidof@gmail.com>
2022-10-04 09:17:15 +0200

    Update server.lua

--- qb-coords/server.lua
+++ qb-coords/server.lua
@@ -4,4 +4,4 @@ TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 QBCore.Commands.Add("coords", "See map coords", {}, false, function(source, args)
         TriggerClientEvent("qb-coords_client:enablecoords", -1)
-    end)
+end)
